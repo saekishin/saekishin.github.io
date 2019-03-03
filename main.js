d6c3155f6cdaffbede57419a557f917c7c681641
Shin Saeki <shin.saeki123@gmail.com>
2019-03-03 01:35:25 +0000

    i

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+//
 
+(function() {
+  'use strict';
+
+//documentのidがstartのオブジェクトを取得する
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  var result = document.getElementById('result');
+  
+//クリックしたときの時間を取得するための変数
+  var startTime;
+//上の代数startに対し「clickした場合」のイベントリスナを設定している
+start.addEventListener('click', function() {
+  
+});
+
+stop.addEventListener('click', function() {
+  
+});
+  
+  
+  
+  
+  
+})();
