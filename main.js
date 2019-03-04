c793a56c18a8a6a93e0477e00f3e4949fbfdd7de
saekishin <43920663+saekishin@users.noreply.github.com>
2019-03-04 09:14:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,16 +14,20 @@
 //上の代数startに対し「clickした場合のイベントリスナを設定している　⇒対象オブジェクト.addEventListner(イベントの条件、function)
 start.addEventListener('click', function() {
 //ここにイベントの処理を記述する
-//
+//時間はDate.nowでしゅとくする
   starTime = Date.now();
 
 });
 
 stop.addEventListener('click', function() {
-  
+//経過時間を取得する
+  var elapsedTime;
+//ミリ秒なので、1000で割り算する
+  elapsedTime = (Date.now() - startTime) / 1000
 });
   
-  
+//上で宣言した変数resultに対し、elapsedTime1を投入（toFixedで小数点～桁まで表示させる）
+  result.textContent = elapsedTime.toFixed(3);
   
   
   
